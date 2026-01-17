1ea1c29d01e5f26a54365ecf32cc65fd1efd5807
ashish <pariseashishbannu@email.com>
2026-01-17 18:37:43 +0530

    comit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1';
+const API_URL = 'http://api.pariseashish.com:8000/api/v1';
 
 const api: AxiosInstance = axios.create({
     baseURL: API_URL,
